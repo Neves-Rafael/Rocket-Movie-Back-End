9e3abf8c685010f24a7914f2c13f69e819653eb2
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-06 14:11:24 -0300

    :sparkles: Feat a migration automation to create a new table in the database

--- src/server.js
+++ src/server.js
@@ -2,17 +2,19 @@
 require("express-async-errors");
 
 //importando conexão com o Sqlite
-const database = require("./database/sqlite")
+// const database = require("./database/sqlite") -- substituido pelas migrations
+
+//importando as migrations
+const migrationsRun = require("./database/sqlite/migrations");
 
 //importando AppError
-const AppError = require("./utils/AppError")
+const AppError = require("./utils/AppError");
 
 //coletando o express e atribuindo a uma const
 const express = require("express");
 
 //importando as rotas
-const routes = require("./routes")
-
+const routes = require("./routes");
 
 //inicializando o express
 const app = express();
@@ -23,30 +25,28 @@ app.use(express.json());
 app.use(routes);
 
 //inicializando o database
-database();
+// database(); -- substituido pelas migrations
+migrationsRun();
 
 //Verificação geral de errors
 //manter sintaxe mesmo se request e next não forem utilizados por exemplo
 app.use((error, request, response, next) => {
-    //verificação de error por parte do cliente
-    if(error instanceof AppError){
-        //retornando mensagem de error
-        return response.status(error.statusCode).json({
-            status: "Error",
-            message: error.message
-        });
-    }
+  //verificação de error por parte do cliente
+  if (error instanceof AppError) {
+    //retornando mensagem de error
+    return response.status(error.statusCode).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
 
-    console.error(error);
+  console.error(error);
 
-    return response.status(500).json({
-        status: "Error",
-        message: "Internal server Error"
-    })
-})
-
-
-
+  return response.status(500).json({
+    status: "Error",
+    message: "Internal server Error",
+  });
+});
 
 //Porta onde será inicializado
 const PORT = 3333;
