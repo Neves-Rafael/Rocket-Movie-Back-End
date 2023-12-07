ef6fa014d59037df32668440ca4f778d8d3973f1
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-07 18:55:08 -0300

    :heavy_plus_sign: Add e config Knex.js Dependency

---
+++ src/database/knex/index.js
@@ -0,0 +1,9 @@
+//importando a configuração do knex para dentro da src
+const config = require("../../../knexfile");
+const knex = require("knex");
+
+//passando as configurações de conexão
+const connection = knex(config.development)
+
+//Exportamos agora a conexão para ser utilziada em toda aplicação
+module.exports = connection;
