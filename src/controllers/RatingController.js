0bff7fbcb5eafe3217acf6171b9701c74c16cad6
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-09 11:18:56 -0300

    :bug: Fix rating calculate

--- src/controllers/RatingController.js
+++ src/controllers/RatingController.js
@@ -1,22 +1,27 @@
 const knex = require("../database/knex")
 
 class RatingController{
+    //Função para calcular todas as "notas" e retornar uma media
     async index(request, response){
-        const { id} = request.params;
 
-        const rating = await knex("rating").select("stars")
+        const rating = await knex("rating").select("stars");
         let result;
-        for (let i = 0; i < rating.length - 1; i++) {
-            let sumOne = rating[i].stars
-            let sumTwo = rating[i+1].stars
-            result = (sumOne + sumTwo) / rating.length
-
+        //Loop para calcular os valores baseado no tamanho da lista
+        if(rating.length < 2){
+            return response.json({
+                message: "A media está em:", rating
+            })
+        }else{
+            for (let i = 0; i < rating.length - 1; i++) {
+                let sumOne = rating[i].stars
+                let sumTwo = rating[i+1].stars
+                console.log(sumOne)
+                result = (sumOne + sumTwo) / rating.length
+            }
+            return response.json({
+                message: "A media está em:", result
+            })
         }
-        console.log(result)
-
-        return response.json({
-            message: "A media está em:", result
-        })
     }
 }
 
--- src/routes/rating.routes.js
+++ src/routes/rating.routes.js
@@ -13,7 +13,7 @@ const ratingController = new RatingController();
 //Fazendo uma requisição com o post chamando o MovieNotesController.create para gerar um novo usuário
 //Query Params
 
-ratingRoutes.get("/:user_id", ratingController.index);
+ratingRoutes.get("/", ratingController.index);
 
 
 //exportando para quem quiser utilizar
