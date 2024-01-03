90991ea8d07d17d830bcb0bf788bb2e08c32d7e4
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-03 17:54:51 -0300

    :bulb: Docs update comentary

--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -1,17 +1,16 @@
 const sqlite3 = require("sqlite3");
 const sqlite = require("sqlite");
-//Biblioteca já inserida com o node
+//Biblioteca já inserida com o node para resolver os endereços dos arquivos em qualquer sistema operacional;
 const path = require("path");
 
-//Adicionando conexão com o banco de dados (necessário ser Async já que ocorre em momentos diferentes)
+//Adicionando conexão com o banco de dados (necessário ser Async já que ocorre em momentos diferentes);
 
-//Caso não exista o DB, ele irá criar um novo
-
+//Caso não exista o DB, ele irá criar um novo;
 async function sqliteConnection() {
   const database = await sqlite.open({
-    //Utilizando o path adicionamos um caminho para a criação do nosso DB caso ele ainda não exista
+    //Utilizando o path adicionamos um caminho para a criação do nosso DB caso ele ainda não exista;
     filename: path.resolve(__dirname, "..", "database.db"),
-    //adicionando o driver para "rodar o sqlite"
+    //adicionando o driver para "rodar o sqlite";
     driver: sqlite3.Database,
   });
 
