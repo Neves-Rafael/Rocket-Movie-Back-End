0d9a607b7712c87ef73af4f3a8f8a44af8fed168
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-03 20:24:39 -0300

    :bulb: Docs update comentary

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -30,6 +30,7 @@ class UsersController {
       );
     }
 
+    //Criptografando a senha;
     const hashedPassword = await hash(password, 8);
 
     //inserindo usuários na tabela
@@ -43,19 +44,19 @@ class UsersController {
     });
   }
 
-  //atualização do usuário
+  //atualização do usuário;
   async update(request, response) {
-    //pegando os parâmetros
+    //pegando os parâmetros;
     const { old_password, new_password, email, name } = request.body;
     const { id } = request.params;
 
-    //conectando com o DB
+    //conectando com o DB;
     const database = await sqliteConnection();
 
-    //Buscando o usuário
+    //Buscando o usuário;
     const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
 
-    //Verificando se o usuário existe
+    //Verificando se o usuário existe;
     if (!user) {
       throw new AppError(
         "Usuário não encontrado, verifique seu ID e tente novamente",
@@ -63,45 +64,45 @@ class UsersController {
       );
     }
 
-    //buscando email
+    //buscando email;
     const userWithUpdatedEmail = await database.get(
       "SELECT * FROM users WHERE email = (?)",
       [email]
     );
 
-    //verificando se o email alterado já está existente
+    //verificando se o email alterado já está existente;
     if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
       throw new AppError("Este email já está em uso, tente novamente!");
     }
 
-    //se chegou até aqui após todas as etapas de verificação significa que podemos atualizar o usuário
-    //Fazemos uma verificação se o name ou email não forem passados com nullish operator
+    //se chegou até aqui após todas as etapas de verificação significa que podemos atualizar o usuário;
+    //Fazemos uma verificação se o name ou email não forem passados com nullish operator;
     user.name = name ?? user.name;
     user.email = email ?? user.email;
 
-    //verificação se possui a senha
+    //verificação se possui a senha;
     if (new_password && !old_password) {
       throw new AppError(
         "Você precisa adicionar a senha antiga para atualizar!"
       );
     }
 
-    //verificando se as 2 senhas existem
+    //verificando se as 2 senhas existem;
     if (new_password && old_password) {
-      //comparando se as senhas são iguais
+      //comparando se as senhas são iguais;
       const checkPassword = await compare(old_password, user.password);
 
-      //se a senha não for igual
+      //se a senha não for igual;
       if (!checkPassword) {
         throw new AppError("A senha antiga não confere");
       }
 
-      //se passou por todas as etapas de verificação atualizamos a senha
+      //se passou por todas as etapas de verificação atualizamos a senha;
       user.password = await hash(new_password, 8);
     }
 
-    //enviando a atualização dos dados atualizados
-    //Passando o datetime por uma função do banco de dados
+    //enviando a atualização dos dados atualizados;
+    //Passando o datetime por uma função do banco de dados;
     await database.run(
       `
       UPDATE users SET
@@ -114,7 +115,7 @@ class UsersController {
       [user.name, user.email, user.password, user.id]
     );
 
-    //resposta de confirmação
+    //resposta de confirmação;
     return response.status(200).json({
       message: "Usuário atualizado com sucesso",
     });
