17dc2e3cb7d32458654d982626493eb79bb21034
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-04 18:08:59 -0300

    :recycle: Refactor RatingController index function

--- src/controllers/RatingController.js
+++ src/controllers/RatingController.js
@@ -4,7 +4,7 @@ class RatingController {
   //Função para calcular todas as "notas" e retornar uma media
   async index(request, response) {
     const rating = await knex("rating").select("stars");
-    let result;
+
     //Loop para calcular os valores baseado no tamanho da lista
     if (rating.length < 2) {
       return response.json({
@@ -12,15 +12,15 @@ class RatingController {
         rating,
       });
     } else {
-      for (let i = 0; i < rating.length - 1; i++) {
-        let sumOne = rating[i].stars;
-        let sumTwo = rating[i + 1].stars;
-        console.log(sumOne);
-        result = (sumOne + sumTwo) / rating.length;
+      let sum = 0;
+      for (let i = 0; i < rating.length; i++) {
+        sum += rating[i].stars;
       }
+
+      let result = sum / rating.length;
       return response.json({
         message: "A media está em:",
-        result,
+        result: result,
       });
     }
   }
