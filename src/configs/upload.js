f7240f85ff838aafc95ae6eb7f860a773233b3b8
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-05 16:09:24 -0300

    :bulb: Docs add comentary in upload file

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,17 +1,31 @@
+//para resolver o caminho de arquivos
 const path = require("path");
+
+//importação da biblioteca multer para fazer o upload
 const multer = require("multer");
+
+//importação do crypto que já está disponível dentro do node
 const crypto = require("crypto");
 
-const TMP_FOLDER = path.resolve(__dirname, "..", "tmp");
+//caminho da pasta temp
+const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
+//caminho da pasta de upload
 const UPLOAD_FOLDER = path.resolve(__dirname, "uploads");
 
 const MULTER = {
+  //propriedade do multer de storage
   storage: multer.diskStorage({
+    //destino onde será salvo o arquivo
     destination: TMP_FOLDER,
+
+    //função para dar nome ao arquivo
     filename: (request, file, callback) => {
+      //criando o hash de forma aleatória para dar nome ao arquivo
       const fileHash = crypto.randomBytes(10).toString("hex");
+      //criando o nome do arquivo com o hash e o nome original
       const fileName = `${fileHash}-${file.originalname}`;
 
+      //retornando a callback com o filename
       return callback(null, fileName);
     },
   }),
@@ -21,4 +35,4 @@ module.exports = {
   TMP_FOLDER,
   UPLOAD_FOLDER,
   MULTER,
-}
+};
