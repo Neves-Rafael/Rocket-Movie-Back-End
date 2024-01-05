b8cb4daaf80120e0f920b26b72f1e0c37b3c4fdd
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-05 12:41:20 -0300

    :sparkles: Feat user validation

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,10 +1,19 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
 //classe para controlar a seção de autenticação
 class SessionsController {
-    //criando a conexão do usuário
-    async create(request, response) {
-        const { email, password } = request.body;
-        return response.json({email, password});
+  //criando a conexão do usuário
+  async create(request, response) {
+    const { email, password } = request.body;
+
+    const user = await knex("users").where({ email }).first();
+
+    if(!user){
+        throw new AppError('E-mail ou senha inválidos', 401);
     }
+
+    return response.json({ user });
+  }
 }
 
 module.exports = SessionsController;
--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -1,12 +1,12 @@
 //criação das rotas de seção para verificar se o usuário está ou não autenticado;
 
-const { Router} = require("express");
+const { Router } = require("express");
 
 //importando o sessionsController e instanciando a classe;
 const SessionsController = require("../controllers/SessionsController");
 const sessionsControllers = new SessionsController();
 
-//enviando a rota / para o controller 
+//enviando a rota / para o controller
 const sessionsRoutes = Router();
 sessionsRoutes.post("/", sessionsControllers.create);
 
