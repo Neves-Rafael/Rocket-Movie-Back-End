6ab800fc24116eb71f41268c73b80659b5b08c13
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-09 17:14:53 -0300

    :sparkles: Feat groupBy in TagsController

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -7,7 +7,7 @@ class TagsController {
     const user_id = request.user.id;
 
     //buscando as tags
-    const tags = await knex("tags").where({ user_id });
+    const tags = await knex("tags").where({ user_id }).groupBy("name");
 
     return response.json(tags);
   }
