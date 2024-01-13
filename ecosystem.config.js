0e0f149899535faa214b03163585fa0d26a1877a
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-13 19:59:36 -0300

    :sparkles: Feat config in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,15 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  apps: [
+    {
+      name: "app",
+      script: "./src/server.js",
+      instance: "max",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
